649399ef1c99ced5347620969ab0f471d9c803a6
snake <admin@viasnake.com>
2024-11-16 23:36:45 +0900

    support homebrew

--- linux/.config/fish/config.fish
+++ linux/.config/fish/config.fish
@@ -1,4 +1,11 @@
 if status is-interactive
   # linuxbrew
-  eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+  if test -d /home/linuxbrew/.linuxbrew
+    eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+  end
+
+  # homebrew
+  if test -d /opt/homebrew
+    eval (/opt/homebrew/bin/brew shellenv)
+  end
 end
